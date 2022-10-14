566d0c6da8c57c993c9abc1ca31821a42995f189
Uqba Ahmad <104266403+Uqbaahmad@users.noreply.github.com>
2022-10-14 14:57:27 +0530

    commit

---
+++ Prediction/Homepage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Customer Churn Analysis and Prediction",
+    page_icon="╰(*°▽°*)╯╰(*°▽°*)╯"
+)
+
+st.title("Main Page")
+st.sidebar.success("Select a page above")
